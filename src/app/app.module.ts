100a5ec59c38d3c72e7bfed6825f1d09985a1810
miyazakimaiko <myzkmik19922@gmail.com>
2021-07-06 17:26:54 +0100

    Implemented igx tab ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { IgxTabsModule } from 'igniteui-angular';
 
 import { AppComponent } from './app.component';
 import { WeatherWidgetMainComponent } from './weather-widget-main.component';
-import { WeatherDetailGeneralCompornent } from './weather-detail-general.component'
+import { WeatherDetailGeneralCompornent } from './weather-detail-general.component';
 import { WeatherDataService } from './weather-data.service';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { locationList, locationListToken } from './providers';
 
 @NgModule({
   declarations: [
@@ -15,11 +18,15 @@ import { WeatherDataService } from './weather-data.service';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    IgxTabsModule
   ],
   providers: [
-    WeatherDataService
+    WeatherDataService,
+    { provide: locationListToken, useValue: locationList }
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
