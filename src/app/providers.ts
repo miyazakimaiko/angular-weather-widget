f58203c9efebd9ed098c96831283e4254324b916
miyazakimaiko <myzkmik19922@gmail.com>
2021-07-06 19:08:18 +0100

    Changed city choices

--- src/app/providers.ts
+++ src/app/providers.ts
@@ -4,10 +4,7 @@ export const locationListToken = new InjectionToken('lookupListToken');
 
 export const locationList: any[] = [
   { text: "Dublin, Ireland", searchValue: "Dublin,ie",  timezone: "Eire"},
-  { text: "London, England", searchValue: "London,gb",  timezone: "Europe/Belfast"},
-  { text: "Paris, France", searchValue: "Paris,fr",  timezone: "Europe/Paris"},
   { text: "Berlin, Germany", searchValue: "Berlin,de",  timezone: "Europe/Berlin"},
-  { text: "Warsaw, Poland", searchValue: "Warsaw,pl",  timezone: "Poland"},
   { text: "Helsinki, Finland", searchValue: "Helsinki,fi",  timezone: "Europe/Helsinki"},
   { text: "Cairo, Egypt", searchValue: "Cairo,eg",  timezone: "Egypt"},
   { text: "Lagos, Nigeria", searchValue: "Lagos,ng",  timezone: "Africa/Lagos"},
@@ -15,7 +12,9 @@ export const locationList: any[] = [
   { text: "New Delhi, India", searchValue: "New Delhi,in",  timezone: "Asia/Calcutta"},
   { text: "Bangkok, Thailand", searchValue: "Bangkok,th",  timezone: "Asia/Bangkok"},
   { text: "Tashkent, Uzbekistan", searchValue: "Tashkent,uz",  timezone: "Asia/Tashkent"},
-  { text: "Moscow, Russia", searchValue: "Moscow,ru",  timezone: "Europe/Moscow"},
   { text: "Tokyo, Japan", searchValue: "Tokyo,jp",  timezone: "Asia/Tokyo"},
+  { text: "Moscow, Russia", searchValue: "Moscow,ru",  timezone: "Europe/Moscow"},
+  { text: "Toronto, Canada", searchValue: "Toronto,ca",  timezone: "America/Toronto"},
+  { text: "Mexico City, Mexico", searchValue: "Mexico City,mx",  timezone: "America/Mexico_City"},
   { text: "Brazilia, Brazil", searchValue: "Brazilia,br",  timezone: "America/Sao_Paulo"}
 ];
